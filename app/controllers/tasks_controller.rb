38a21484d8e0c53c35e51a2b6b73b571cae370df
takara.iwaya <nasubihuzimi7@gmail.com>
2020-11-03 13:37:54 +0000

    loginkadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,10 @@ class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show]
     
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(5)
+        if logged_in?
+        @task = current_user.tasks.build
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
+        end
     end
 
     def show
@@ -14,7 +17,7 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = "task が正常に投稿されました"
             redirect_to @task
